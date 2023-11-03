a8b2c2eef67aecf36e36c89014deac0cbe46130d
sgalanb <73245438+sgalanb@users.noreply.github.com>
2023-11-03 11:21:52 -0300

    fix analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
             {children}
           </main>
           <Footer />
-          <Analytics />
         </div>
+        <Analytics />
       </body>
     </html>
   )
