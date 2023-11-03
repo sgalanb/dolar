0e1e92c05dd21fd562baef38a2d29ff09ea8f7af
sgalanb <73245438+sgalanb@users.noreply.github.com>
2023-11-03 10:55:22 -0300

    remove calculadora

--- app/calculadora/page.tsx
+++
@@ -1,5 +0,0 @@
-export default async function Calculadora() {
-  return (
-    <div className="flex w-full flex-col items-center justify-center gap-4"></div>
-  )
-}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,5 @@
 'use client'
 
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
-} from '@/components/ui/navigation-menu'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
@@ -41,53 +34,13 @@ export default function Header() {
           <Link href="/">
             <p className="text-2xl font-bold">DolarYa</p>
           </Link>
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <Link href="/" legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Cotizaciones
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href="/calculadora" legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Calculadora
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
         </div>
 
         <div className="hidden h-20 w-full items-center justify-center p-3 lg:flex">
           <div className="flex gap-9">
-            <NavigationMenu>
-              <NavigationMenuList>
-                <NavigationMenuItem>
-                  <Link href="/" legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      Cotizaciones
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-                <Link href="/" className="px-8">
-                  <p className="text-2xl font-bold">DolarYa</p>
-                </Link>
-                <NavigationMenuItem>
-                  <Link href="/calculadora" legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      Calculadora
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
+            <Link href="/" className="px-8">
+              <p className="text-2xl font-bold">DolarYa</p>
+            </Link>
           </div>
         </div>
       </nav>
