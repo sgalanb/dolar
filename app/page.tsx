ab496761dc6ce69d43c372df3578fcf8b36fc78a
sgalanb <73245438+sgalanb@users.noreply.github.com>
2023-11-02 21:21:30 -0300

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import { dolar } from '@/app/api/update-prices/route'
 import { kv } from '@vercel/kv'
 
-export const revalidate = 60
-
 export default async function Home() {
   const dolar: dolar | null = await kv.get('dolar')
 
   return (
     <div className="flex w-full flex-col items-center justify-center gap-4">
       {/* Dólar Oficial */}
-      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 p-4 shadow">
+      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 bg-white p-4 shadow">
         <h2 className="font-bold">Oficial</h2>
         <div className="flex items-center justify-center gap-4">
           <div className="flex flex-col items-center justify-center gap-1">
@@ -23,7 +21,7 @@ export default async function Home() {
         </div>
       </div>
       {/* Dólar tarjeta/turista/solidario/ahorro */}
-      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 p-4 shadow">
+      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 bg-white p-4 shadow">
         <h2 className="font-bold">Solidario</h2>
         <div className="flex flex-col items-center justify-center gap-1">
           <h3>Compra/Venta</h3>
@@ -31,7 +29,7 @@ export default async function Home() {
         </div>
       </div>
       {/* Dólar Blue */}
-      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 p-4 shadow">
+      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 bg-white p-4 shadow">
         <h2 className="font-bold">Blue</h2>
         <div className="flex items-center justify-center gap-4">
           <div className="flex flex-col items-center justify-center gap-1">
@@ -45,7 +43,7 @@ export default async function Home() {
         </div>
       </div>
       {/* Dólar MEP */}
-      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 p-4 shadow">
+      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 bg-white p-4 shadow">
         <h2 className="font-bold">MEP</h2>
         <div className="flex flex-col items-center justify-center gap-1">
           <h3>Compra/Venta</h3>
@@ -53,7 +51,7 @@ export default async function Home() {
         </div>
       </div>
       {/* Dólar MEP 24/7 */}
-      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 p-4 shadow">
+      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 bg-white p-4 shadow">
         <h2 className="font-bold">MEP 24/7</h2>
         <div className="flex items-center justify-center gap-4">
           <div className="flex flex-col items-center justify-center gap-1">
@@ -67,7 +65,7 @@ export default async function Home() {
         </div>
       </div>
       {/* Dólar CCL */}
-      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 p-4 shadow">
+      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 bg-white p-4 shadow">
         <h2 className="font-bold">CCL</h2>
         <div className="flex flex-col items-center justify-center gap-2">
           <h3>Compra/Venta</h3>
@@ -75,12 +73,12 @@ export default async function Home() {
         </div>
       </div>
       {/* Dólar Cripto */}
-      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 p-4 shadow">
+      <div className="flex w-full items-center justify-between rounded-2xl border border-zinc-100 bg-white p-4 shadow">
         <h2 className="font-bold">Cripto</h2>
         <div className="flex items-center justify-center gap-4">
           <div className="flex flex-col items-center justify-center gap-2">
             <h3>Comprá a</h3>
-            <p className="font-bold">{`$${dolar?.cripto.ask.toFixed(2)}`}</p>
+            <p className="font-bold">{`${dolar?.cripto.ask.toFixed(2)}`}</p>
           </div>
           <div className="flex flex-col items-center justify-center gap-2">
             <h3>Vendé a</h3>
--- components/Header.tsx
+++ components/Header.tsx
@@ -10,89 +10,7 @@ import {
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
-const productos: { title: string; href: string; description: string }[] = [
-  {
-    title: 'Dólar MEP',
-    href: '/dolar-mep',
-    description:
-      'Comprá y vendé dólares MEP cualquier día, a cualquier hora y con un botón.',
-  },
-  {
-    title: 'Obligaciones Negociables',
-    href: '/on',
-    description: 'Opera obligaciones negociables en pesos o dólares.',
-  },
-  {
-    title: 'CEDEARs',
-    href: '/cedears',
-    description:
-      'Invertí en acciones extranjeras en pesos o dólares sin comisiones.',
-  },
-  {
-    title: 'Bonos Públicos',
-    href: '/bonos',
-    description:
-      'Financiá la deuda de una entidad pública y cobrá una renta fija.',
-  },
-  {
-    title: 'Acciones',
-    href: '/acciones',
-    description: 'Invertí en acciones Argentinas sin comisiones.',
-  },
-  {
-    title: 'Letras',
-    href: '/letras',
-    description:
-      'Financiá una deuda pública a corto plazo y cobrá una renta fija.',
-  },
-  {
-    title: 'Fondos Comunes de Inversión',
-    href: '/fci',
-    description: 'Invertí en fondos comunes de inversión en pesos o dólares.',
-  },
-  {
-    title: 'Cauciones',
-    href: '/cauciones',
-    description:
-      'Prestale dinero a otros inversores del mercado y recibí intereses a cambio.',
-  },
-]
-
-const ayuda: { title: string; href: string; description: string }[] = [
-  {
-    title: 'Preguntas Frecuentes',
-    href: '/preguntas-frecuentes',
-    description:
-      'Resolvemos todas tus dudas y te ayudamos a sacarle el máximo provecho a Cocos.',
-  },
-  {
-    title: 'Transferencias',
-    href: '/transferencias',
-    description: 'Instrucciones para realizar transferencias.',
-  },
-  {
-    title: 'Tarifas y Comisiones',
-    href: '/tarifas-y-comisiones',
-    description:
-      'Conocé las tarifas y comisiones para operar en Cocos Capital.',
-  },
-]
-
-const empresa: { title: string; href: string; description: string }[] = [
-  {
-    title: 'Quiénes Somos',
-    href: '/quienes-somos',
-    description: 'Conocé más sobre nosotros.',
-  },
-  {
-    title: 'Trabajá Con Nosotros',
-    href: 'https://cocoscapital.recruitee.com',
-    description: 'Conocé las vacantes que tenemos disponibles.',
-  },
-]
-
 export default function Header() {
-  const [isOpenMobileHeader, setIsOpenMobileHeader] = useState<boolean>(false)
   const [isScrolled, setIsScrolled] = useState<boolean>(false)
 
   useEffect(() => {
@@ -116,15 +34,11 @@ export default function Header() {
     <header
       className={`sticky top-0 z-50 border-b ${
         isScrolled ? 'border-gray-500/10' : 'border-transparent'
-      } ${
-        isOpenMobileHeader
-          ? 'bg-white'
-          : 'bg-white transition-colors duration-200'
-      }`}
+      } bg-white transition-colors duration-200`}
     >
       <nav className="mx-auto w-full max-w-7xl">
         <div className="flex h-20 w-full items-center justify-between p-3 lg:hidden">
-          <Link href="/" onClick={() => setIsOpenMobileHeader(false)}>
+          <Link href="/">
             <p className="text-2xl font-bold">DolarYa</p>
           </Link>
           <NavigationMenu>
