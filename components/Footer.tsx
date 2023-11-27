072e34631a2ecc550d6bbe5366030f60cae52053
sgalanb <73245438+sgalanb@users.noreply.github.com>
2023-11-27 12:37:51 -0300

    add ios app to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,8 @@ import InstagramButton from '@/components/InstagramButton'
 import TwitterButton from '@/components/TwitterButton'
 import { Separator } from '@/components/ui/separator'
 import dynamic from 'next/dynamic'
+import Image from 'next/image'
+import Link from 'next/link'
 import Balancer from 'react-wrap-balancer'
 
 const ThemeToggle = dynamic(() => import('@/components/ThemeToggle'), {
@@ -19,39 +21,32 @@ export default function Footer() {
       <div className="mx-auto flex w-full max-w-3xl flex-col items-center justify-center gap-6 px-3 py-7">
         <div className="flex w-full flex-col items-center justify-center gap-6 md:flex-row md:justify-between">
           <div className="flex flex-col items-center justify-center gap-9 md:flex-row md:gap-0">
-            <div className="flex flex-col items-center justify-center gap-2 md:items-start">
+            <div className="flex flex-col items-center justify-center gap-1 md:items-start">
               <p className="text-lg">Seguí las cotizaciones en redes</p>
               <div className="flex items-center justify-center gap-6">
                 <TwitterButton />
                 <InstagramButton />
               </div>
-            </div>
-
-            {/* <div className="flex flex-col items-center justify-center gap-2">
-              <p className="text-lg">Bajate la app con widgets</p>
-              <div className="flex items-center justify-center gap-6">
-                <Link href="/" className="flex h-8 items-center justify-center">
-                  <Image
-                    src="/social-icons/apple-icon.svg"
-                    alt="descárgalo en google play"
-                    width={20}
-                    height={20}
-                    className="hover:opacity-70 dark:invert"
-                  />
-                </Link>
-                <Link href="/" className="flex h-8 items-center justify-center">
-                  <Image
-                    src="/social-icons/android-icon.svg"
-                    alt="descárgalo en el app store"
-                    width={20}
-                    height={20}
-                    className="hover:opacity-70 dark:invert"
-                  />
-                </Link>
+              <div className="flex flex-col items-center justify-center gap-1 pt-3 md:items-start">
+                <p className="text-lg">Bajate la app con widgets</p>
+                <div className="flex items-center justify-center gap-6">
+                  <Link
+                    href="https://apps.apple.com/ar/app/d%C3%B3larya-precio-del-d%C3%B3lar-hoy/id6472918637?platform=iphone"
+                    className="flex h-8 items-center justify-center"
+                    target="_blank"
+                  >
+                    <Image
+                      src="/social-icons/apple-icon.svg"
+                      alt="descárgalo en el app store"
+                      width={20}
+                      height={20}
+                      className="hover:opacity-70 dark:invert"
+                    />
+                  </Link>
+                </div>
               </div>
-            </div> */}
+            </div>
           </div>
-
           <ThemeToggle />
         </div>
 
