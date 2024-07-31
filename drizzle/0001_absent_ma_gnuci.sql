1d4186a79342a809cbe61c024de1c969e326e6a8
sgalanb <73245438+sgalanb@users.noreply.github.com>
2024-07-31 15:58:51 -0300

    remove drizzle files

--- drizzle/0001_absent_ma_gnuci.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "historical-prices" ALTER COLUMN "timestamp" SET DATA TYPE timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "historical-prices" ALTER COLUMN "timestamp" SET DEFAULT now();
